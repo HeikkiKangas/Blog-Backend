d331d77a3f4f1dddd4d473f82f1bc259d613d268
Heikki Kangas <heikki.kangas@tuni.fi>
2020-03-10 21:24:03 +0200

    Add RESTController.

---
+++ src/main/java/fi/tuni/tamk/tiko/bmb/blogbackend/controller/RESTController.java
@@ -0,0 +1,58 @@
+package fi.tuni.tamk.tiko.bmb.blogbackend.controller;
+
+import fi.tuni.tamk.tiko.bmb.blogbackend.model.BlogPost;
+import fi.tuni.tamk.tiko.bmb.blogbackend.repositories.BlogPostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+public class RESTController {
+    @Autowired
+    BlogPostRepository blogPostRepository;
+
+    @GetMapping("/posts")
+    @Transactional
+    public Iterable<BlogPost> getPosts() {
+        return blogPostRepository.findAll();
+    }
+
+    @GetMapping("/posts/{id}")
+    @Transactional
+    public Optional<BlogPost> getPost(@PathVariable long id) {
+        return blogPostRepository.findById(id);
+    }
+
+    @PostMapping("/posts")
+    @Transactional
+    public ResponseEntity<BlogPost> addBlogPost(@RequestBody BlogPost b, UriComponentsBuilder uri) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(uri.path("/posts/{id}").buildAndExpand(b.getId()).toUri());
+        blogPostRepository.save(b);
+        return new ResponseEntity<>(b, headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/posts/{id}")
+    @Transactional
+    public ResponseEntity<BlogPost> updateBlogPost(@RequestBody BlogPost b, UriComponentsBuilder uri) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(uri.path("/posts/{id}").buildAndExpand(b.getId()).toUri());
+        return new ResponseEntity<>(blogPostRepository.save(b), headers, HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("/posts/{id}")
+    @Transactional
+    public ResponseEntity<Void> deleteBlogPost(@PathVariable long id) {
+        blogPostRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+
+}
